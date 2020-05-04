ab2e2da76eb5a3e1d03bbbe2b9a411fcc5a53ee4
clareduerden <clare.duerden@gmail.com>
2020-05-04 13:50:18 +0100

    Complete mobile menu styles that show on toggle

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.querySelector(".site-header__menu-icon");
     this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
     this.events();
   }
 
@@ -15,6 +16,7 @@ class MobileMenu {
   toggleTheMenu() {
     // the toggle method belongs to the classList object will toggle the class specified
     this.menuContent.classList.toggle("site-header__menu-content--is-visible");
+    this.siteHeader.classList.toggle("site-header--is-expanded");
   }
 }
 
